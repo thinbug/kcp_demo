e699010f9e3a318fff713f42fddd9a1b94b9e5a6
thinbug <thinbug@163.com>
2023-10-20 19:33:36 +0800

    linux编译，拷贝so到server

--- KcpClient/Program.cs
+++ KcpClient/Program.cs
@@ -4,8 +4,9 @@ using static NetLibrary.KCP;
 
 
 KcpSocketClient server = new KcpSocketClient();
-server.Create("127.0.0.1", 11001); 
-//server.Create("192.168.3.86", 11001);
+//server.Create("127.0.0.1", 11001);
+server.Create("112.126.169.192", 11001);
+
 Task.Run(async () =>
 {
     while (true)
